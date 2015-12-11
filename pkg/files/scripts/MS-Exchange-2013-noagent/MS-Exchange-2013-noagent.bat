3cb4cfc13ed1ef32c45a9665d2fc8294e3d1bd4a
Robert Vandervoort <bigrobintx@gmail.com>
2015-12-11 14:41:08 -0600

    fixed another type

--- pkg/files/scripts/MS-Exchange-2013-noagent/MS-Exchange-2013-noagent.bat
+++ pkg/files/scripts/MS-Exchange-2013-noagent/MS-Exchange-2013-noagent.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-Powershell -ExecutionPolicy Bypass -f ..\..\plugins\scripts\MS-Exchange-2013-noagent\MS-Exchange-2013-noagent.ps1
+Powershell -ExecutionPolicy Bypass -f ..\..\plugins\scripts\MS-Exchange-2013-noagent\MS-Exchange-2013-noagent1.2.ps1
